bfa795a001daeb6d611635d595b66908c1b01543
pauljustine.ardiente@cit.edu <jameslewel.padecio@cit.edu>
2023-05-05 12:57:15 +0800

    Add Console App Project

---
+++ MLApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
