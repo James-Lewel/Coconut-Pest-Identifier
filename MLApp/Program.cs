439f3b9e988c588b3cf1060c57e8c662184e1d29
pauljustine.ardiente@cit.edu <jameslewel.padecio@cit.edu>
2023-05-05 23:44:03 +0800

    Update solution to include machine learning model

--- MLApp/Program.cs
+++ MLApp/Program.cs
@@ -6,7 +6,19 @@ namespace MLApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Load sample data
+            var sampleData = new MLModel.ModelInput()
+            {
+                ImageSource = @"D:\Projects_C#\Cocunot-Pest-Identifier\Coconut Leaf Images\CCI_Caterpillars\CCI_10_0_jpg.rf.041b1c4c936db7346c0e1cc66e687757.jpg",
+            };
+
+
+            var result = MLModel.Predict(sampleData);
+
+
+            Console.Clear();
+            Console.WriteLine("Prediction is: ");
+            Console.WriteLine(result.Prediction);
         }
     }
 }
