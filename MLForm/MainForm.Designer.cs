a285041c7a7d28c1535a7e0745b09735c1028b62
pauljustine.ardiente@cit.edu <jameslewel.padecio@cit.edu>
2023-05-06 19:12:36 +0800

    Add Windows Form Project

---
+++ MLForm/MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MLForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.cameraComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.informationLabel = new System.Windows.Forms.Label();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.cameraDisplay = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.splitContainer.Location = new System.Drawing.Point(12, 15);
+            this.splitContainer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.cameraComboBox);
+            this.splitContainer.Panel1.Controls.Add(this.label2);
+            this.splitContainer.Panel1.Controls.Add(this.label1);
+            this.splitContainer.Panel1.Controls.Add(this.informationLabel);
+            this.splitContainer.Panel1.Controls.Add(this.uploadButton);
+            this.splitContainer.Panel1.Controls.Add(this.resetButton);
+            this.splitContainer.Panel1.Controls.Add(this.stopButton);
+            this.splitContainer.Panel1.Controls.Add(this.startButton);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.cameraDisplay);
+            this.splitContainer.Size = new System.Drawing.Size(758, 661);
+            this.splitContainer.SplitterDistance = 187;
+            this.splitContainer.TabIndex = 3;
+            // 
+            // cameraComboBox
+            // 
+            this.cameraComboBox.FormattingEnabled = true;
+            this.cameraComboBox.Location = new System.Drawing.Point(3, 215);
+            this.cameraComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cameraComboBox.Name = "cameraComboBox";
+            this.cameraComboBox.Size = new System.Drawing.Size(182, 28);
+            this.cameraComboBox.TabIndex = 4;
+            this.cameraComboBox.SelectedIndexChanged += new System.EventHandler(this.cameraComboBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(5, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Buttons :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(5, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Camera :";
+            // 
+            // informationLabel
+            // 
+            this.informationLabel.AutoSize = true;
+            this.informationLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.informationLabel.Location = new System.Drawing.Point(5, 306);
+            this.informationLabel.MaximumSize = new System.Drawing.Size(179, 0);
+            this.informationLabel.Name = "informationLabel";
+            this.informationLabel.Size = new System.Drawing.Size(98, 20);
+            this.informationLabel.TabIndex = 3;
+            this.informationLabel.Text = "Information : ";
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.BackColor = System.Drawing.SystemColors.Control;
+            this.uploadButton.Location = new System.Drawing.Point(3, 252);
+            this.uploadButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(182, 38);
+            this.uploadButton.TabIndex = 1;
+            this.uploadButton.Text = "Upload";
+            this.uploadButton.UseVisualStyleBackColor = false;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.SystemColors.Control;
+            this.resetButton.Location = new System.Drawing.Point(3, 131);
+            this.resetButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(182, 38);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.SystemColors.Control;
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(3, 86);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(182, 38);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(3, 41);
+            this.startButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(182, 38);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // cameraDisplay
+            // 
+            this.cameraDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cameraDisplay.Location = new System.Drawing.Point(3, 4);
+            this.cameraDisplay.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cameraDisplay.Name = "cameraDisplay";
+            this.cameraDisplay.Size = new System.Drawing.Size(560, 653);
+            this.cameraDisplay.TabIndex = 0;
+            this.cameraDisplay.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(782, 691);
+            this.Controls.Add(this.splitContainer);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 738);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Coconut Pest Identifier";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.cameraDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Label informationLabel;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox cameraDisplay;
+        private System.Windows.Forms.ComboBox cameraComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ MLForm/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using MLApp;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Size = OpenCvSharp.Size;
+
+namespace MLForms
+{
+    public partial class MainForm : Form
+    {
+        private VideoCapture capture;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+
+            capture = new VideoCapture(0);
+            Mat frame = new Mat();
+            
+            while (true)
+            {
+                capture.Read(frame);
+
+                if (frame.Empty())
+                {
+                    break;
+                }
+
+                // Resize the frame to match the size of the PictureBox
+                Cv2.Resize(frame, frame, new Size(cameraDisplay.Width, cameraDisplay.Height));
+                cameraDisplay.Image = BitmapConverter.ToBitmap(frame);
+
+                Application.DoEvents();
+
+                if (capture == null)
+                {
+                    cameraDisplay.Image = null;
+                    break;
+                }
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+
+            capture.Dispose();
+            capture = null;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Open Image";
+                openFileDialog.Filter = "Image Files(*.jpeg;*.bmp;*.png;*.jpg)|*.jpeg;*.bmp;*.png;*.jpg";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Load the image
+                    Bitmap originalImage = new Bitmap(openFileDialog.FileName);
+
+                    // Resize the image to fit the picture box
+                    Bitmap resizedImage = new Bitmap(cameraDisplay.Width, cameraDisplay.Height);
+                    Graphics graphics = Graphics.FromImage(resizedImage);
+                    graphics.DrawImage(originalImage, 0, 0, cameraDisplay.Width, cameraDisplay.Height);
+
+                    // Display the resized image in the picture box
+                    cameraDisplay.Image = resizedImage;
+
+                    // Use the image path for MLModel input
+                    var sampleData = new MLModel.ModelInput()
+                    {
+                        ImageSource = openFileDialog.FileName,
+                    };
+
+                    var result = MLModel.Predict(sampleData);
+
+                    informationLabel.Text = "Information : \n" +
+                                             result.Prediction;
+                }
+            }
+        }
+
+        private void cameraComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
